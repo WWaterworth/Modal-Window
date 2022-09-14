b1d8797c206f4805cb30081b722239990b54dd46
Warren Waterworth <warren.waterworth@gmail.com>
2022-09-14 06:21:53 -0600

    refactored. added comments

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
+//Add event listener to each button, removing hidden class on click
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
 }
+
+//adds hidden class to modal window and overlay
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
